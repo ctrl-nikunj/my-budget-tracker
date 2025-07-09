07c7cc81d393571283e02eb42b0d9c525ec26023
ctrl-nikunj <goyalnikunj2004@gmail.com>
2025-07-09 16:22:30 +0530

    Restructure: moved project into backend/ and frontend/

--- src/pages/Transactions.jsx
+++
@@ -1,8 +0,0 @@
-export default function Transactions() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Transactions</h1>
-      <p>Your transactions will appear here.</p>
-    </div>
-  );
-}
